8299e9a640c7537bb0b9da68f412450777cbf678
Nik Mathur <mathurn@berkeley.edu>
2017-06-05 15:45:53 -0700

    Added Home and Help pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-  resources :microposts
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'application#home'
 end
